d7f440d875ad91d7349d9d3ce01b6624c9e49294
rozakgit <muhammadabdulrozak21@gmail.com>
2025-10-27 05:57:13 +0700

    Create MaterialApp with Title = Tugas Week 5

--- lib/main.dart
+++ lib/main.dart
@@ -3,3 +3,16 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MyApp());
 }
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Tugas Week 5',
+      debugShowCheckedModeBanner: false,
+      home: Halaman(),
+    );
+  }
+}
